dfc0c99ce8b8dcfcd5cb9d19a4d39cfee32c1c2c
Jacky Chan <libing.chen@gmail.com>
2012-08-22 14:29:30 +0800

    Unitils Redis Module

--- unitils-redis/src/main/java/org/unitils/nosql/redis/RedisModule.java
+++ unitils-redis/src/main/java/org/unitils/nosql/redis/RedisModule.java
@@ -1,8 +1,16 @@
 package org.unitils.nosql.redis;
 
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
 import org.unitils.core.Module;
 import org.unitils.core.TestListener;
+import org.unitils.nosql.redis.annotation.RedisDataSet;
+import org.unitils.util.AnnotationUtils;
+import redis.clients.jedis.Jedis;
 
+import java.lang.reflect.Method;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -10,14 +18,25 @@ import java.util.Properties;
  *
  * @author linux_china
  */
+@SuppressWarnings("unchecked")
 public class RedisModule implements Module {
+    /**
+     * jedis
+     */
+    private Jedis jedis = null;
+
     /**
      * Initializes the module with the given configuration settings.
      *
      * @param configuration The config, not null
      */
     public void init(Properties configuration) {
-
+        String host = configuration.getProperty("redis.host");
+        String port = configuration.getProperty("redis.port");
+        if (port == null) {
+            port = "6379";
+        }
+        jedis = new Jedis(host, Integer.valueOf(port));
     }
 
     /**
@@ -34,6 +53,61 @@ public class RedisModule implements Module {
      * @return The test listener, not null
      */
     public TestListener getTestListener() {
-        return null;
+        return new RedisListener();
+    }
+
+    /**
+     * Test listener that is called while the test framework is running tests
+     */
+    protected class RedisListener extends TestListener {
+        /**
+         * Invoked before any of the test in a test class are run.
+         * This can be overridden to for example add test-class initialization.
+         *
+         * @param testObject The test class, not null
+         */
+        @Override
+        public void afterCreateTestObject(Object testObject) {
+            RedisDataSet redisDataSet = AnnotationUtils.getClassLevelAnnotation(RedisDataSet.class, testObject.getClass());
+            if (redisDataSet != null) {
+                fillValuesToRedis(redisDataSet.value());
+            }
+        }
+
+        @Override
+        public void beforeTestSetUp(Object testObject, Method testMethod) {
+            RedisDataSet redisDataSet = testMethod.getAnnotation(RedisDataSet.class);
+            if (redisDataSet != null) {
+                fillValuesToRedis(redisDataSet.value());
+            }
+        }
+
+        /**
+         * fill values to redis
+         *
+         * @param locations locations
+         */
+        @SuppressWarnings({"ConstantConditions"})
+        private void fillValuesToRedis(String[] locations) {
+            if (locations != null && locations.length > 0) {
+                for (String location : locations) {
+                    try {
+                        SAXBuilder builder = new SAXBuilder();
+                        Document document = builder.build(this.getClass().getResourceAsStream(location));
+                        List<Element> keyElements = document.getRootElement().getChildren("key");
+                        for (Element keyElement : keyElements) {
+                            String key = keyElement.getAttributeValue("name");
+                            String value = keyElement.getTextTrim();
+                            if (key != null && value != null) {
+                                jedis.set(key, value);
+                            }
+                        }
+                    } catch (Exception ignore) {
+
+                    }
+                }
+            }
+        }
+
     }
 }
