d07ca7e1337726adc76c2b01ef44992487ca77b8
Jacky Chan <libing.chen@gmail.com>
2012-08-22 14:29:57 +0800

    add value fileds

--- unitils-redis/src/main/java/org/unitils/nosql/redis/annotation/RedisDataSet.java
+++ unitils-redis/src/main/java/org/unitils/nosql/redis/annotation/RedisDataSet.java
@@ -11,4 +11,12 @@ import java.lang.annotation.*;
 @Retention(value = RetentionPolicy.RUNTIME)
 @Inherited
 public @interface RedisDataSet {
+    /**
+     * The file name of the data set. If left empty, the default filename will
+     * be used: first 'classname'.'testMethodname'.xml will be tried, if that file does not exist,
+     * 'classname'.xml is tried. If that file also does not exist, an exception is thrown.
+     *
+     * @return the fileName, empty for default
+     */
+    String[] value() default {};
 }
